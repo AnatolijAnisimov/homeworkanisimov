5be35027c7bdce3901b8c22113c9b642a5a7ebdf
AnatolijAnisimov <anatolij.anisimov2000@gmail.com>
2022-06-05 12:52:25 +0300

    homework

--- src/test/java/getpricehomework/ComponetsPrice.java
+++ src/test/java/getpricehomework/ComponetsPrice.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.List;
 
 
@@ -12,6 +16,7 @@ public class ComponetsPrice {
         private final String MENU_ITEM_TO_OPEN = "Datoru komponentes, tīkla produkti";
         private final String CATEGORY_TO_OPEN = "Atmiņa, HDD un SSD";
         private final String SUBCAT_TO_OPEN = "Operatīvā atmiņa";
+        private final By ACCEPT_COOKIES_BTN = By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll");
         private final By MENU = By.xpath(".//div[@class = 'submenu-lvl1 submenu-lvl1--invisible submenu-lvl1--index']");
         private final By MENU_ITEM = By.xpath(".//li[contains(@class, 'submenu-lvl1__list-item--has-child')]");
         private final By CATEGORY_MENU = By.xpath(".//div[@class = 'filter-block only-desktop']");
@@ -26,6 +31,10 @@ public class ComponetsPrice {
             WebDriver browser = new ChromeDriver();
             browser.manage().window().maximize();
             browser.get("http://www.1a.lv");
+            WebDriverWait wait = new WebDriverWait (browser, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
+
+            browser.findElement(ACCEPT_COOKIES_BTN).click();
 
             List<WebElement> items = browser.findElement(MENU).findElements(MENU_ITEM);
             for (WebElement web : items) {
