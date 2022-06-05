3199cd0952fded03ddb86e71b45f531c712f55c8
AnatolijAnisimov <anatolij.anisimov2000@gmail.com>
2022-06-05 12:45:16 +0300

    homework

---
+++ src/test/java/getpricehomework/ComponetsPrice.java
@@ -0,0 +1,57 @@
+package getpricehomework;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+
+
+public class ComponetsPrice {
+        private final String MENU_ITEM_TO_OPEN = "Datoru komponentes, tīkla produkti";
+        private final String CATEGORY_TO_OPEN = "Atmiņa, HDD un SSD";
+        private final String SUBCAT_TO_OPEN = "Operatīvā atmiņa";
+        private final By MENU = By.xpath(".//div[@class = 'submenu-lvl1 submenu-lvl1--invisible submenu-lvl1--index']");
+        private final By MENU_ITEM = By.xpath(".//li[contains(@class, 'submenu-lvl1__list-item--has-child')]");
+        private final By CATEGORY_MENU = By.xpath(".//div[@class = 'filter-block only-desktop']");
+        private final By CATEGORY_ITEM = By.xpath(".//a[contains(@href, 'datoru-komponentes-tikla-produkti')]");
+        private final By SUBCAT_MENU = By.xpath(".//div[@class = 'block block-category-list']");
+        private final By SUBCAT_ITEM = By.xpath(".//li[contains(@class,'new-cat-item')]");
+
+
+        @Test
+        public void WebPage() {
+            System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+            WebDriver browser = new ChromeDriver();
+            browser.manage().window().maximize();
+            browser.get("http://www.1a.lv");
+
+            List<WebElement> items = browser.findElement(MENU).findElements(MENU_ITEM);
+            for (WebElement web : items) {
+                if (web.getText().equals(MENU_ITEM_TO_OPEN)) {
+                    web.click();
+                    break;
+                }
+            }
+
+            List<WebElement> catItems = browser.findElement(CATEGORY_MENU).findElements(CATEGORY_ITEM);
+            for (WebElement web : catItems) {
+                if (web.getText().contains(CATEGORY_TO_OPEN)) {
+                    web.click();
+                    break;
+                }
+            }
+
+            List<WebElement> subcatItems = browser.findElement(SUBCAT_MENU).findElements(SUBCAT_ITEM);
+            for (WebElement web : subcatItems) {
+                if (web.getText().contains(SUBCAT_TO_OPEN)) {
+                    web.click();
+                    break;
+                }
+            }
+
+
+        }
+    }
+
